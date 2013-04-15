f33901c98d5b2e65e79cd8a7e05f55f25a4b0b8a
Phil Brown <philip22brown@gmail.com>
2013-04-15 16:59:13 -0500

    committing gh-pages so I can use the portfolio

--- js/config.js
+++ js/config.js
@@ -6,51 +6,51 @@
 	/**
 	 * Your Name
 	 */
-	config.name = "My Name";
+	config.name = "Phil Brown";
 	/**
 	 * Your title - such as "Software Developer" or "Web Designer"
 	 */
-	config.title = "My Job Title";
+	config.title = "Mobile Developer";
 	/**
 	 * Some summary about who you are or what you do
 	 */
-	config.summary = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+	config.summary = "I created this website! More soon.";
 	/**
 	 * Name of the company you work for
 	 */
-	config.company = "current employer";
+	config.company = "North Pole Engineering";
 	
 	/**
 	 * Email address. Any format. This will also be used to retrieve your Gravatar, unless the gravatar hash is
 	 * set
 	 */
-	config.email = "foobar@example.com";
+	config.email = null;
 	/**
 	 * Phone number. Any Format
 	 */
-	config.phone = "555-555-5555";
+	config.phone = null;
 	/**
 	 * Name of the location where you live or work. Be as vague or specific as you like. Ex: "Minnesota, USA"
 	 */
-	config.location = null;
+	config.location = "Minnesota, USA";
 	
 	/**
 	 * This is your GitHub username
 	 */
-	config.github = null;
+	config.github = "phil-brown";
 	/**
 	 * This is your twitter handle. You can include the @, but don't have to
 	 */
-	config.twitter = null;
+	config.twitter = "philip22brown";
 	
 	/**
 	 * Stack Overflow profile ID. this is the number before your username if you look at your profile
 	 */
-	config.stackoverflow = null;
+	config.stackoverflow = "763080";
 	/**
 	 * URL to your linkedIn public profile
 	 */
-	config.linkedin = null;
+	config.linkedin = "http://www.linkedin.com/pub/phil-brown/22/a08/a65";
 	
 	/** 
 	 * Use this to show the gravatar profile picture if you decide not to provide an email address.
@@ -61,7 +61,7 @@
 	 * </pre>
 	 * or online: http://www.md5hashgenerator.com (with your email address as the input)
 	 */
-	config.gravatar_hash = "";
+	config.gravatar_hash = "e64061a865ef84951bf78944dfecc808";
 	
 	/** Google Analytics: change UA-XXXXX-X to be your site's ID. */
 	config.google_analytics = "UA-XXXXX-X";
